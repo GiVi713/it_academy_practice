b67f7276a4040a3c8cfec882b3dea9ad58401d79
Виктор <lucckyboy713@gmail.com>
2019-12-13 01:22:26 +0300

    Поехали

---
+++ lecture_05/lecture_05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lecture_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            string convert = String.Format("{0:00}:{1:00}:{2:00}",
+            time.Hours, time.Minutes, time.Seconds);
+            if (time.Hours < 12 && time.Hours > 9)
+            {
+                Console.WriteLine(convert + " Good morning, guys");
+            }
+            if (time.Hours > 12 && time.Hours < 15)
+            {
+                Console.WriteLine(convert + " Good day, guys");
+            }
+            if (time.Hours > 15 && time.Hours < 22)
+            {
+                Console.WriteLine(convert + " Good evening, guys");
+            }
+            else
+            {
+                Console.WriteLine(convert + " Good night, lion pride");
+            }//Мне нужно было только понять что можно было TimeSpan представить DateTime и использовать его уже в цикле.
+             //
+        }
+    }
+}
